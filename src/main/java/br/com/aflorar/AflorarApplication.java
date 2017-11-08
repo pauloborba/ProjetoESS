d57cde17bc87ec7c848542cbb244e80cafe763db
Adilson Angelo <aasj2@cin.ufpe.br>
2017-11-07 23:22:57 -0300

    Criação do projeto Spring Boot

---
+++ src/main/java/br/com/aflorar/AflorarApplication.java
@@ -0,0 +1,12 @@
+package br.com.aflorar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AflorarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AflorarApplication.class, args);
+	}
+}
---
+++ src/test/java/br/com/aflorar/Aflorar/AflorarApplicationTests.java
@@ -0,0 +1,16 @@
+package br.com.aflorar.Aflorar;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AflorarApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
