ac1fba2aef972bc2e4b470ba7e3a2d83b9c9bf3a
Adilson Angelo <aasj2@cin.ufpe.br>
2017-12-01 12:58:46 -0300

    #153315993 #153316013 #153316000 Adição de dependencias no module

--- gui/aflorar/src/app/app.module.ts
+++ gui/aflorar/src/app/app.module.ts
@@ -1,19 +1,50 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 import { MaterializeModule } from 'angular2-materialize';
 
 import { AppComponent } from './app.component';
+import { ProdutoComponent } from './produto/component/produto.component';
+import { MarcaComponent } from './marca/component/marca.component';
+import { CategoriaComponent } from './categoria/component/categoria.component';
+import { ProdutoService } from './produto/service/produto.service';
+import { CategoriaService } from './categoria/service/categoria.service';
+import { MarcaService } from './marca/service/marca.service';
+import { ProdutoFormComponent } from './produto/produto-form/produto-form.component';
+// import { NavBarComponent } from './nav-bar/component/nav-bar.component';
+
+const appRoutes: Routes = [
+  { path: 'produtos', component: ProdutoComponent },
+  { path: 'produtos/novo', component: ProdutoFormComponent },
+  { path: 'categorias', component: CategoriaComponent },
+  { path: 'marcas', component: MarcaComponent },
+ // { path: '**', component: PageNotFoundComponent }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProdutoComponent,
+    MarcaComponent,
+    CategoriaComponent,
+    ProdutoFormComponent,
+    // NavBarComponent,
   ],
   imports: [
     BrowserModule,
-    MaterializeModule
+    MaterializeModule,
+    FormsModule,
+    RouterModule.forRoot(
+      appRoutes
+    )
   ],
-  providers: [],
+  providers: [
+    ProdutoService,
+    MarcaService,
+    CategoriaService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
