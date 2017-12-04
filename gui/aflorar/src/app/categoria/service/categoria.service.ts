1851b0d5d6078be8e4efcbae8b1d67e5f90b165e
Adilson Angelo <aasj2@cin.ufpe.br>
2017-12-04 06:06:58 -0300

    #153315993 - Implementação CRUD de categorias GUI

--- gui/aflorar/src/app/categoria/service/categoria.service.ts
+++ gui/aflorar/src/app/categoria/service/categoria.service.ts
@@ -1,8 +1,55 @@
+import { Categoria } from './../categoria';
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import * as global from '../../global';
 
 @Injectable()
 export class CategoriaService {
 
-  constructor() { }
+  private url = global.url + '/categoria';
 
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  getAllCategorias(): Promise<Categoria[]>{
+    return this.http.get(this.url + '/get_all')
+      .toPromise()
+      .then(res => {
+        return res;
+      })
+      .catch(this.tratarErro);
+  }
+
+  updateCategoria(categoria: Categoria){
+    return this.http.put(this.url + '/edit', categoria)
+      .toPromise()
+      .then(res =>{
+        return res;
+      })
+      .catch(this.tratarErro);
+  }
+
+  addNewCategoria(categoria: Categoria){
+    return this.http.post(this.url + '/new', categoria)
+      .toPromise()
+      .then(res =>{
+        return res;
+      })
+      .catch(this.tratarErro);
+  }
+
+  deleteCategoria(categoriaId: string){
+    return this.http.delete(this.url + '/delete/' + categoriaId)
+      .toPromise()
+      .then(res =>{
+        return res
+      })
+      .catch(this.tratarErro);
+  }
+
+  private tratarErro(err: any): Promise<any>{
+    console.error("Acesso mal sucedido ao serviço de categorias", err);
+    return Promise.reject(err.message || err);
+  }
 }
