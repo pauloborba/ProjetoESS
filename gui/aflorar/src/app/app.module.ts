52f8003d2fb74b18fe02edcd2ff32e56700d10c4
=Joao Santos <jvos@cin.ufpe.br>
2017-12-04 05:10:45 -0300

    #153358396 Rever rotas no front

--- gui/aflorar/src/app/app.module.ts
+++ gui/aflorar/src/app/app.module.ts
@@ -1,39 +1,86 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import { HttpClientModule } from '@angular/common/http';
 
 import { MaterializeModule } from 'angular2-materialize';
 
 import { AppComponent } from './app.component';
+import { ProdutoComponent } from './produto/component/produto.component';
+import { MarcaComponent } from './marca/component/marca.component';
+import { CategoriaComponent } from './categoria/component/categoria.component';
+import { ProdutoService } from './produto/service/produto.service';
+import { CategoriaService } from './categoria/service/categoria.service';
+import { MarcaService } from './marca/service/marca.service';
+import { ProdutoFormComponent } from './produto/produto-form/produto-form.component';
+import { DashboardComponent } from './dashboard/component/dashboard.component';
+import { NavBarComponent } from './nav-bar/component/nav-bar.component';
+import { HomeClienteComponent } from './home-cliente/home-cliente.component';
+import { HomeClienteService } from './home-cliente/service/home-cliente.service';
+
+import { ClienteComponent } from './cliente/cliente.component';
+import { PaisComponent } from './pais/pais.component';
+import { ClienteService } from './cliente/service/cliente.service';
+import { PaisService } from './pais/service/pais.service';
+
+import { ClienteFormComponent } from './cliente/cliente-form/cliente-form.component';
+
+import { LoginComponent } from './login/login.component';
+
+import { LoginService } from './login/service/login.service';
 import { CarrinhoComponent } from './carrinho/carrinho.component';
-import { NavbarComponent } from './navbar/navbar.component';
-import { ProdutoClienteComponent } from './produto-cliente/produto-cliente.component';
 
 const appRoutes: Routes = [
-  { path: 'carrinho', component: CarrinhoComponent },
-  { path: 'produto', component: ProdutoClienteComponent },
-//  { path: 'produtos', component: ProdutoComponent },
-//  { path: 'produtos/novo', component: ProdutoFormComponent },
- // { path: 'categorias', component: CategoriaComponent },
- // { path: 'marcas', component: MarcaComponent },
-  { path: '**', component: ProdutoClienteComponent }
+  { path: 'produtos', component: ProdutoComponent },
+  { path: 'produtos/novo', component: ProdutoFormComponent },
+  { path: 'categorias', component: CategoriaComponent },
+  { path: 'marcas', component: MarcaComponent },
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'home', component: HomeClienteComponent  },
+  { path: 'cliente', component: ClienteComponent },
+  { path: 'adicionar', component: ClienteFormComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'carrinho', component: CarrinhoComponent }
+//  { path: '**', component: PageNotFoundComponent }
 ];
 
 @NgModule({
   declarations: [
     AppComponent,
+    ProdutoComponent,
+    MarcaComponent,
+    CategoriaComponent,
+    ProdutoFormComponent,
+    DashboardComponent,
+    NavBarComponent,
+    HomeClienteComponent,
+    ClienteComponent,
+    PaisComponent,
+    ClienteFormComponent,
+    LoginComponent,
     CarrinhoComponent,
-    NavbarComponent,
-    ProdutoClienteComponent
   ],
   imports: [
     BrowserModule,
     MaterializeModule,
+    FormsModule,
     RouterModule.forRoot(
-      appRoutes,
-    )
+      appRoutes
+    ),
+    HttpModule,
+    HttpClientModule,
   ],
-  providers: [],
+  providers: [
+    ProdutoService,
+    MarcaService,
+    CategoriaService,
+    HomeClienteService,
+    ClienteService,
+    PaisService,
+    LoginService,
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
